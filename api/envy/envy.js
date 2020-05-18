48dfbcfcdb61a61fa646789a7d3b23e55d0212cc
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2020-05-18 19:28:29 +0430

    testing static file serving

--- api/envy/envy.js
+++ api/envy/envy.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
+
 exports.handler = async (event, context, callback) => {
-  console.log(process.env.CHROME);
-  return { statusCode: 200, body: process.env.CHROME || 'undefined' };
+  const test = fs.readdirSync('./');
+  return { statusCode: 200, body: JSON.stringify(test) || 'undefined' };
 };
